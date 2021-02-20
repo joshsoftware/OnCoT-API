f641fc430f318386b9d6b280c029b6c8e0376ad2
Atira Bagwan <atira.bagwan@joshsoftware.com>
2021-02-20 16:07:22 +0530

    Added all model resources to app/admin

---
+++ app/admin/test_cases.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register TestCase do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :input, :output, :marks, :problem_id, :created_by_id, :updated_by_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:input, :output, :marks, :problem_id, :created_by_id, :updated_by_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
