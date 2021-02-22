1a027f89d53854fa3a745328e852e91f8657b6bf
atirabagwan <46757055+atirabagwan@users.noreply.github.com>
2021-02-22 16:52:33 +0530

    Judge0 api (#1)

---
+++ app/controllers/languages_controller.rb
@@ -0,0 +1,17 @@
+class LanguagesController < ApplicationController
+  def index
+    response = JudgeZeroApi.new.get('/languages')
+    render json: response
+  end
+  
+  def all
+    response = JudgeZeroApi.new.get('/languages/all')
+    render json: response
+  end
+
+  def show
+    id = params[:id]
+    response = JudgeZeroApi.new(params[:id]).get('/languages')
+    render json: response.to_s
+  end
+end
