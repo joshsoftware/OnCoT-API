82eb84b2b05d8dc7ff4315e3926a7a1907f80fde
Samruddhi Deshpande <samruddhideshpande133@gmail.com>
2021-02-24 11:30:38 +0530

    unexpected admin folder deleted

--- app/admin/problems.rb
+++
@@ -1,6 +0,0 @@
-ActiveAdmin.register Problem do
-  permit_params do
-    permitted_params = params.permit(:title, :description, :organization_id)
-    permitted_params.merge!(created_by_id: current_admin_user.id, updated_by_id: current_admin_user.id)
-  end
-end
