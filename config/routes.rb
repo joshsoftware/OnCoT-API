51196a9f977056d4ff75746cef3c0fa58424c80d
Samruddhi Deshpande <samruddhideshpande133@gmail.com>
2021-02-25 11:12:00 +0530

    Unexpected active admin gem completely removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :statuses, only: [:index]
 
-  resources :languages, only: [:index, :show] do
-    get 'all', on: :collection
-  end
 end
