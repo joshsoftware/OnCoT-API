ced547218329499c18ffb9aa52791c91f661d27a
Samruddhi Deshpande <samruddhideshpande133@gmail.com>
2021-02-26 11:29:45 +0530

    association added

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,5 +1,5 @@
 class Problem < ApplicationRecord
-  # belongs_to :user
+  belongs_to :user
   belongs_to :updated_by, class_name: 'User', foreign_key: 'updated_by_id'
   belongs_to :created_by, class_name: 'User', foreign_key: 'created_by_id'
   belongs_to :organization
