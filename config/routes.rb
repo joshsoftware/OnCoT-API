8db1a32b5d6e6d1b317ab704e79ec05d19e19453
Atira Bagwan <atira.bagwan@joshsoftware.com>
2021-02-27 14:05:12 +0530

    added drive start time

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :statuses, only: [:index]
 
-  get '/drive/:id', to "drives#drive_time_left"
+  get '/drive/:id', to: "drives#drive_time_left"
 
   resources :languages, only: [:index, :show] do
     get 'all', on: :collection
