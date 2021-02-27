2477b4af9ef3e1087646b36e67c20e92b509897b
Atira Bagwan <atira.bagwan@joshsoftware.com>
2021-02-27 14:04:16 +0530

    added drive start time

--- app/controllers/drives_controller.rb
+++ app/controllers/drives_controller.rb
@@ -1,2 +1,15 @@
-class DrivesController < ApplicationController
+class DrivesController < ApiController
+  def drive_time_left
+    @drive = Drive.find(params[:id])
+
+    drive_time = @drive.start_time.localtime
+    current_time = DateTime.now.localtime
+
+    time_left = drive_time - current_time
+    if time_left < 0
+      render json: {message: "drive already started"}
+    else
+      render json: {time_left: time_left}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :statuses, only: [:index]
 
+  get '/drive/:id', to "drives#drive_time_left"
+
   resources :languages, only: [:index, :show] do
     get 'all', on: :collection
   end
