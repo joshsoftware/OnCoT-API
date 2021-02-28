ea51f95fca1b4c895458ffb030b60da57efcbba1
Atira Bagwan <atira.bagwan@joshsoftware.com>
2021-02-28 12:58:11 +0530

    added render_success and render_error

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -6,4 +6,13 @@ class ApiController < ActionController::API
   def record_not_found
     render json: { message: '404 Not found' }, status: 404
   end
+
+  def render_success(data: nil, message: nil)
+    render json: { message: message, data: data }, status: 200
+  end
+
+  def render_error(message: nil)
+    render json: message, status: 400
+  end
+
 end
--- app/controllers/drives_controller.rb
+++ app/controllers/drives_controller.rb
@@ -1,15 +1,19 @@
 class DrivesController < ApiController
   def drive_time_left
     @drive = Drive.find(params[:id])
+    if DateTime.now.utc > @drive.end_time
+      render_error(message: 'drive ended')
+      return
+    end
 
     drive_time = @drive.start_time.localtime
     current_time = DateTime.now.localtime
 
     time_left = drive_time - current_time
     if time_left.negative?
-      render json: { message: 'drive already started' }
+      render_success(message: 'drive already started')
     else
-      render json: { time_left: time_left }
+      render_success(data: time_left, message: 'timeleft')
     end
   end
 end
