7067530da8ea2d99c01f8498a6ac2fbe022a1cbb
Samruddhi Deshpande <samruddhideshpande133@gmail.com>
2021-03-04 12:35:48 +0530

    rotes modified

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :statuses, only: [:index]
-  resources :problems, only: [:show] do
-    get 'problems_path', to: 'problems#show'
-  end
+  resources :problems, only: [:show] 
   resources :languages, only: %i[index show] do
     get 'all', on: :collection
   end
