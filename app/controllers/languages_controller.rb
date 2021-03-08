a5195e3d016ce411cf733324644fcbf45c0a62cb
Somnath <Somnath.surwase@joshsoftware.com>
2021-03-08 11:54:38 +0530

    Sent languages data in json format

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,12 +1,14 @@
 class LanguagesController < ApplicationController
   def index
     response = JudgeZeroApi.new.get('/languages')
-    render json: response
+    body = JSON.parse(response.body)
+    render json: body
   end
   
   def all
     response = JudgeZeroApi.new.get('/languages/all')
-    render json: response
+    body = JSON.parse(response.body)
+    render json: body
   end
 
   def show
