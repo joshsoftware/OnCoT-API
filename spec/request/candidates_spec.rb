cc75ed069577089ff057ea4cd2f568e108d60674
Samruddhi Deshpande <samruddhideshpande133@gmail.com>
2021-03-09 11:39:02 +0530

    candidate tesing added

--- spec/request/candidates_spec.rb
+++ spec/request/candidates_spec.rb
@@ -4,18 +4,22 @@ RSpec.describe CandidatesController, type: :controller do
   context 'candidates#update' do
     let(:organization) { create(:organization) }
     let(:user) { create(:user) }
+    let(:candidate) { create(:candidate) }
     let(:drive) do
       create(:drive, updated_by_id: user.id, created_by_id: user.id,
                      organization: organization)
     end
+   
+    let(:drives_candidates) { create(:drives_candidates, drive_id: drive.id,candidate_id: candidate.id) }
 
-    let(:candidate) { create(:candidate, drive_id: drive.id) }
+    # let(:candidate) { create(:candidate, drive_id: drive.id) }
 
     it 'update the candidate details' do
-      get :update, params: { id: candidate.id }
+      get :update, params: { id: drives_candidates.candidate_id }
 
       expect(response.body).to eq({ data: candidate, message: 'Success' }.to_json)
       expect(response).to have_http_status(200)
     end
   end
 end
+
