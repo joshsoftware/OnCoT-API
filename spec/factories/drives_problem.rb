e44829ac59d4bb3a431bc0e72c9fee40d19cee3e
Samruddhi Deshpande <samruddhideshpande133@gmail.com>
2021-03-09 11:53:23 +0530

    testing added

---
+++ spec/factories/drives_problem.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory :drives_problem, class: DrivesProblem do
+   
+      created_at { Time.now }
+      updated_at { Time.now }
+    end
+  end
+  
--- spec/request/problems_spec.rb
+++ spec/request/problems_spec.rb
@@ -4,13 +4,19 @@ RSpec.describe ProblemsController, type: :controller do
   context 'problem#show' do
     let(:organization) { create(:organization) }
     let(:user) { create(:user) }
+    let(:drive) do
+      create(:drive, updated_by_id: user.id, created_by_id: user.id,
+                     organization: organization)
+    end
+
     let(:problem) do
       create(:problem, updated_by_id: user.id, created_by_id: user.id,
                        organization: organization)
     end
+    let(:drives_problem) { create(:drives_problem, drive_id: drive.id , problem_id: problem.id) }
 
     it 'returns the problem data' do
-      get :show, params: { id: problem.id }
+      get :show, params: { id: drives_problem.problem_id }
 
       expect(response.body).to eq({ data: problem, message: 'Success' }.to_json)
       expect(response).to have_http_status(200)
