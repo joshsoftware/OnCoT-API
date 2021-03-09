a1b43c161cb1877c224100d8f76e7f041cced20b
Somnath <Somnath.surwase@joshsoftware.com>
2021-03-09 19:26:01 +0530

    bug fixed

--- app/controllers/api/v1/admin/problems_controller.rb
+++ app/controllers/api/v1/admin/problems_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::Admin::ProblemsController < ApiController
   def create
     problem = Problem.new(problem_params)
     if problem.save
-      render_success(data: {problem: serialize_resource( problem, ProblemSerializer )}, message: I18n.t('create.success', model_name: Problem))
+      render_success(data: {problem:  problem}, message: I18n.t('create.success', model_name: Problem))
     else
       render_error(message:problem.errors.full_messages)
     end
@@ -13,32 +13,32 @@ class Api::V1::Admin::ProblemsController < ApiController
     problems = Problem.all
 
     if problems
-      render_success(data: {problems: serialize_resource( problems, ProblemSerializer ) }, message: I18n.t('index.success', model_name: Problem))
+      render_success(data:  {problems:  problems} , message: I18n.t('index.success', model_name: Problem))
     else
       render_error(message:'Problems not exist')
     end
   end
 
   def show
-    problem = Problem.find_by_id(params[:id])
+    problem = Problem.find_by(id: params[:id])
     if problem
-      render_success(data: {problem: serialize_resource( problem, ProblemSerializer ) }, message: I18n.t('show.success', model_name: Problem))
+      render_success(data: {problem:  problem}  , message: I18n.t('show.success', model_name: Problem))
     else
       render_error(message:'Problems not exist')
     end
   end
 
   def update
-    problem = Problem.find_by_id(params[:id])
+    problem = Problem.find_by(id: params[:id])
     if problem.update(problem_params)
-      render_success(data: {problem: serialize_resource( problem, ProblemSerializer ) }, message: I18n.t('update.success', model_name: Problem))
+      render_success(data: {problem:  problem  }, message: I18n.t('update.success', model_name: Problem))
     else
       render_error(message:'Update failed')
     end
   end
 
   def destroy
-    problem = Problem.find_by_id(params[:id])
+    problem = Problem.find_by(id: params[:id])
     if problem.destroy
       render_success( message: I18n.t('destroy.success', model_name: Problem))
     else
