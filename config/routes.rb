bcbffa4ca3c928651c8b3a56c5046dc24dc58e87
Samruddhi Deshpande <samruddhideshpande133@gmail.com>
2021-03-11 16:58:11 +0530

    Correct routes added

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :statuses, only: [:index]
-  resources :rules, only: [:show]
   resources :languages, only: %i[index show] do
     get 'all', on: :collection
   end
