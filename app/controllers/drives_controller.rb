6676310e18e7eee36ba3caa1fb298c91d606afd3
Sumit-Dhandale <sumitdhandale1999@gmail.com>
2021-03-15 01:33:32 +0530

    added unit testing

--- app/controllers/drives_controller.rb
+++ app/controllers/drives_controller.rb
@@ -1,14 +1,18 @@
+# frozen_string_literal: true
+
 class DrivesController < ApiController
   def show
-    token = params[:token].to_s
+    token = params[:id].to_s
     return render_error(message: I18n.t('token_not_found.message'), status: :not_found) if token.blank?
 
     drive_candidate = DrivesCandidate.find_by(token: token)
+    return render_error(message: I18n.t('not_found.message'), status: :not_found) unless drive_candidate
+
     drive = Drive.find_by(id: drive_candidate.drive_id)
     if drive.present? && drive_candidate.token_valid?
       render_success(data: drive, message: I18n.t('ok.message'))
     else
-      render_error(message: I18n.t('drive_not_found.message'), stats: :not_found)
+      render_error(message: I18n.t('drive_not_found.message'), status: :not_found)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :statuses, only: [:index]
-  resources :candidates, only: [:update] 
+  resources :candidates, only: [:update]
   resources :languages, only: %i[index show] do
     get 'all', on: :collection
   end
-
-  get 'drives/:token', to: 'drives#show'
+  resources :drives, only: [:show]
 end
