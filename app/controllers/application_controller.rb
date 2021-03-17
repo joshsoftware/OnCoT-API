e0bd88307b772025d6fbd31322152dd4cca027f6
Somnath <Somnath.surwase@joshsoftware.com>
2021-03-17 11:29:30 +0530

    devise_auth_token setup in test db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  protect_from_forgery unless: -> { request.format.json? }
 end
