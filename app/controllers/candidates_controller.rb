f433eed39ac4d7cf329247705f92dc33765026b9
Varun Gadde <varungadde99@gmail.com>
2021-03-25 10:06:09 +0530

    Candidate Time and Drive Time Rspec changes

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -26,33 +26,7 @@ class CandidatesController < ApiController
       render_error(message: I18n.t('not_found.message'), status: 404)
     end
   end
-
-  def invite
-    return render_error(I18n.t('blank_input.message'), :unprocessable_entity) if params[:emails].blank?
-
-    emails = params[:emails]
-    candidate_emails = emails.split(',')
-    failed_invitaion ||= []
-    candidate_emails.each do |candidate_email|
-      user = Candidate.create(email: candidate_email)
-      drive_candidate = DrivesCandidate.create(drive_id: params[:drive_id], candidate_id: user.id)
-      next unless user.present? && drive_candidate.present?
-
-      drive_candidate.generate_token!
-      begin
-        CandidateMailer.with(user: user, drive_candidate: drive_candidate).invitation_email.deliver_now
-      rescue StandardError => e
-        failed_invitaion.push(candidate_email)
-      end
-    end
-
-    if failed_invitaion.empty?
-      render_success(message: I18n.t('ok.message'))
-    else
-      render json: { failed_invitaion: failed_invitaion }
-    end
-  end
-
+  
   def candidate_test_time_left
     @drive_candidate.save if @drive_candidate && @drive_candidate.start_time.nil?
 
