442caa289cb420066faf967e3db0f7833c7ccf1e
Varun Gadde <varungadde99@gmail.com>
2021-03-25 15:59:13 +0530

    Added private

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -39,6 +39,8 @@ class CandidatesController < ApiController
               end
     render_success(data: { time_left: time_left }, message: message)
   end
+  
+  private
 
   def candidate_params
     params.permit(:first_name, :last_name, :email, :is_profile_complete, :created_at, :mobile_number,
