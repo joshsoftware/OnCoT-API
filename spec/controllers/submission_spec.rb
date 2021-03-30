e04b33220d464248bb670c7ad7304a08be37acc4
Samruddhi Deshpande <samruddhideshpande133@gmail.com>
2021-03-30 17:55:13 +0530

    API optimized

--- spec/controllers/submission_spec.rb
+++ spec/controllers/submission_spec.rb
@@ -1,22 +1,19 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe SubmissionsController, type: :controller do
   describe 'POST #create' do
     context 'with submission_count greater than 0' do
-      before do
-        header = { 'Accept' => 'application/json' }
+      before  do
         url = 'http://65.1.201.245/submissions/?base64_encoded=false&wait=true'
         stub_request(:post, url)
-          .with(body: { stdin: 'hello', expected_output: 'hello', source_code: "print('hello')",
-                        language_id: 71 }.to_json, headers: header)
-          .to_return(status: 200, body: { stdout: "world\n", status: { description: 'Accepted' } }.to_json)
+          .with( body: { stdin: 'hello', expected_output: 'hello', source_code: "print('hello')",
+                    language_id: 71 }.to_json, headers: headers)
+          .to_return(status: 200, body: {stdout: "world\n",status: { description: 'Accepted'} }.to_json)
 
         stub_request(:post, url)
-          .with(body: { stdin: 'world', expected_output: 'world', source_code: "print('hello')",
-                        language_id: 71 }.to_json, headers:  header)
-          .to_return(status: 200, body: { stdout: "hello\n", status: { description: 'Wrong Answer' } }.to_json)
+          .with( body: { stdin: 'world', expected_output: 'world', source_code: "print('hello')",
+                    language_id: 71 }.to_json,headers:  headers )
+          .to_return(status: 200, body: { stdout: "hello\n", status: {description: 'Wrong Answer'} }.to_json)
 
         organization = create(:organization)
         user = create(:user)
@@ -37,7 +34,7 @@ RSpec.describe SubmissionsController, type: :controller do
         expect(Submission.count).to eq(1)
       end
 
-      it 'returns passed testcases out of total and decremented submission_count' do
+      it 'returns marks with passed testcases and decremented submission_count' do
         result = json
 
         expect(result['data']['passed_testcases']).to eq(1)
@@ -51,7 +48,7 @@ RSpec.describe SubmissionsController, type: :controller do
     context 'with submission_count as 0' do
       it 'returns submission limit exceeded message' do
         headers
-        post :create, params: { source_code: "print('world')", language_id: 71, candidate_id: 1, id: 7, submission_count: 0 }
+        post :create, params: { source_code: "print('world')", language_id: 71, candidate_id: 1, id: 7,submission_count: 0 }
 
         expect(response.body).to eq('submission limit exceeded')
       end
