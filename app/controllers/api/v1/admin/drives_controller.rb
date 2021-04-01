1fb734cbffb554f41f898a403e5744dcb849cd37
Somnath <Somnath.surwase@joshsoftware.com>
2021-04-01 12:44:42 +0530

    Create drive creating entry in drives_problem

--- app/controllers/api/v1/admin/drives_controller.rb
+++ app/controllers/api/v1/admin/drives_controller.rb
@@ -7,8 +7,6 @@ module Api
         before_action :authenticate_user!
         load_and_authorize_resource :drive
         before_action :fetch_drive_data, only: %i[show update]
-        # before_action :fetch_problem_data, only: %i[set_drives_problem create]
-        # after_action :set_drives_problem, only: %i[create update]
 
         def index
           drives = Drive.all
@@ -17,8 +15,8 @@ module Api
         end
 
         def create
-          @drive = Drive.create(drive_params.merge(created_by_id: current_user.id, updated_by_id: current_user.id,
-                                                   organization_id: current_user.organization_id))
+          @drive = Drive.create!(drive_params.merge(created_by_id: current_user.id, updated_by_id: current_user.id,
+                                                    organization_id: current_user.organization_id))
 
           if @drive
             render_success(data: { drive: serialize_resource(@drive, DriveSerializer) },
@@ -29,8 +27,8 @@ module Api
         end
 
         def update
-          if @drive.update(drive_params.merge(updated_by_id: current_user.id,
-                                              organization_id: current_user.organization_id))
+          if @drive.update!(drive_params.merge(updated_by_id: current_user.id,
+                                               organization_id: current_user.organization_id))
             render_success(data: { drive: serialize_resource(@drive, DriveSerializer) },
                            message: I18n.t('update.success', model_name: 'Drive'))
           else
@@ -49,17 +47,8 @@ module Api
           @drive = Drive.find(params[:id])
         end
 
-        # def fetch_problem_data
-        #   # byebug
-        #   @problem = Problem.find(params['drive_problems_attributes']['member'][0]['problem_id'])
-        # end
-
-        # def set_drives_problem
-        #   @drives_problem = DrivesProblem.create(drive_id: @drive.id, problem_id: @problem.id)
-        # end
-
         def drive_params
-          params.permit(:name, :description, :start_time, :end_time, drives_problems_attributes: [])
+          params.permit(:name, :description, :start_time, :end_time, drives_problems_attributes: %i[id problem_id _destroy])
         end
       end
     end
