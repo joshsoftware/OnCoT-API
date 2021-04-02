ca83fcaba534ac95d1bd8ea12614a6df6100e6cf
Samruddhi Deshpande <samruddhideshpande133@gmail.com>
2021-04-02 11:29:34 +0530

    optimized

--- spec/request/problems_spec.rb
+++ spec/request/problems_spec.rb
@@ -5,18 +5,22 @@ require 'rails_helper'
 RSpec.describe ProblemsController, type: :controller do
   describe 'GET index' do
     context 'with correct id ' do
-      before do
-        organization = create(:organization)
-        user = create(:user)
-        drive = create(:drive, updated_by_id: user.id, created_by_id: user.id,
-                               organization_id: organization.id)
-        @problem = create(:problem, updated_by_id: user.id, created_by_id: user.id,
-                                    organization: organization)
-        create(:drives_problem, drive_id: drive.id, problem_id: @problem.id)
-        get :index, params: { id: drive.id }
+      let(:organization) { create(:organization) }
+      let(:user) { create(:user) }
+      let(:drive) do
+        create(:drive, updated_by_id: user.id, created_by_id: user.id,
+                       organization: organization)
       end
+      let(:problem) do
+        create(:problem, updated_by_id: user.id, created_by_id: user.id,
+                         organization: organization)
+      end
+      let(:drives_problem) { create(:drives_problem, drive_id: drive.id, problem_id: problem.id) }
+
       it 'returns the problem data' do
-        expect(response.body).to eq({ data: @problem, message: 'Success' }.to_json)
+        get :index, params: { id: drives_problem.problem_id }
+
+        expect(response.body).to eq({ data: problem, message: 'Success' }.to_json)
         expect(response).to have_http_status(200)
       end
     end
