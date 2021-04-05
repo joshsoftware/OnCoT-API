fef45c13dba3dd7da0e4a87a3fcca5f0b5e925ca
Samruddhi Deshpande <samruddhideshpande133@gmail.com>
2021-04-05 19:38:46 +0530

    migrations changed

--- db/migrate/20210405140625_add_score_and_completed_at_to_drives_candidate.rb
+++ db/migrate/20210405140625_add_score_and_completed_at_to_drives_candidate.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddCompletedAtToDrivesCandidate < ActiveRecord::Migration[6.1]
+class AddScoreAndCompletedAtToDrivesCandidate < ActiveRecord::Migration[6.1]
   def change
     add_column :drives_candidates, :completed_at, :datetime
     add_column :drives_candidates, :score, :integer
