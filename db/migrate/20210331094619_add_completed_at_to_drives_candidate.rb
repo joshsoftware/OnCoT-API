8b91fe5f2992bac4df65d3998d5146a0f4c35b1c
Samruddhi Deshpande <samruddhideshpande133@gmail.com>
2021-04-05 19:29:06 +0530

    migrations added

--- db/migrate/20210331094619_add_completed_at_to_drives_candidate.rb
+++ db/migrate/20210331094619_add_completed_at_to_drives_candidate.rb
@@ -3,5 +3,6 @@
 class AddCompletedAtToDrivesCandidate < ActiveRecord::Migration[6.1]
   def change
     add_column :drives_candidates, :completed_at, :datetime
+    add_column :drives_candidates, :score, :integer 
   end
 end
