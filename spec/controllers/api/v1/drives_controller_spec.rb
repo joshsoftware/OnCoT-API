7d7d56c1fcf210a34f4f8162f1ff102d22515f85
Somnath <Somnath.surwase@joshsoftware.com>
2021-04-07 13:45:40 +0530

    added unit tests for different scenarions of drive time

--- spec/controllers/api/v1/drives_controller_spec.rb
+++ spec/controllers/api/v1/drives_controller_spec.rb
@@ -6,21 +6,34 @@ RSpec.describe Api::V1::DrivesController, type: :controller do
   let(:organization) { create(:organization) }
   let(:role) { create(:role) }
   let(:user) { create(:user, role_id: role.id) }
-  let(:drive) do
+  let(:drive1) do
     create(:drive, updated_by_id: user.id, created_by_id: user.id,
-                   organization: organization)
+                   organization: organization, start_time: DateTime.now.localtime + 1.hours,
+                   end_time: DateTime.now.localtime + 3.hours, duration: 10_800)
+  end
+  let(:drive2) do
+    create(:drive, updated_by_id: user.id, created_by_id: user.id,
+                   organization: organization, start_time: DateTime.now.localtime - 1.hours,
+                   end_time: DateTime.now.localtime + 1.hours, duration: 10_800)
+  end
+  let(:drive3) do
+    create(:drive, updated_by_id: user.id, created_by_id: user.id,
+                   organization: organization, start_time: DateTime.now.localtime - 3.hours,
+                   end_time: DateTime.now.localtime - 1.hours, duration: 10_800)
   end
   let(:candidate) { create(:candidate) }
-  let(:drives_candidate) { create(:drives_candidate, drive_id: drive.id, candidate_id: candidate.id) }
+  let(:drives_candidate1) { create(:drives_candidate, drive_id: drive1.id, candidate_id: candidate.id) }
+  let(:drives_candidate2) { create(:drives_candidate, drive_id: drive2.id, candidate_id: candidate.id) }
+  let(:drives_candidate3) { create(:drives_candidate, drive_id: drive3.id, candidate_id: candidate.id) }
 
   describe 'GET #show' do
     context 'with valid params' do
       it 'returns drive details' do
-        get :show, params: { id: drives_candidate.token }
+        get :show, params: { id: drives_candidate1.token }
 
         data = json
 
-        expect(data['data']['drive']['name']).to eq(drive.name)
+        expect(data['data']['drive']['name']).to eq(drive1.name)
         expect(response).to have_http_status(200)
       end
     end
@@ -37,11 +50,27 @@ RSpec.describe Api::V1::DrivesController, type: :controller do
   describe 'GET #drive_time_left' do
     context 'with valid params' do
       it 'returns time left to start drive' do
-        get :drive_time_left, params: { id: drive.id }
+        get :drive_time_left, params: { id: drive1.id }
 
         data = json
 
         expect(data['message']).to eq(I18n.t('drive.yet_to_start'))
+        expect(response).to have_http_status(200)
+      end
+      it 'returns drive is already started' do
+        get :drive_time_left, params: { id: drive2.id }
+
+        data = json
+
+        expect(data['message']).to eq(I18n.t('drive.started'))
+        expect(response).to have_http_status(200)
+      end
+      it 'returns drive has completed' do
+        get :drive_time_left, params: { id: drive3.id }
+
+        data = json
+
+        expect(data['message']).to eq(I18n.t('drive.ended'))
         expect(response).to have_http_status(200)
       end
     end
