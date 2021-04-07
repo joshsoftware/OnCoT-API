eb653f27b5f1ac2454eaba0f7fd24d946da8a721
Samruddhi Deshpande <samruddhideshpande133@gmail.com>
2021-04-07 13:39:01 +0530

    API modified

--- spec/controllers/api/v1/execution_spec.rb
+++ spec/controllers/api/v1/execution_spec.rb
@@ -4,23 +4,21 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::ExecutionsController, type: :controller do
   describe 'POST #submission_token' do
-    context 'with valid params' do
-      before do
-        stub_request(:post, 'http://65.1.201.245/submissions/?base64_encoded=false&wait=false')
-          .with(body: { source_code: "print('hello')",
-                        language_id: 71 }.to_json, headers: headers)
-          .to_return(status: 200, body: { token: 'token123' }.to_json)
+    before do
+      stub_request(:post, 'http://65.1.201.245/submissions/?base64_encoded=false&wait=false')
+        .with(body: { source_code: "print('hello')",
+                      language_id: 71 }.to_json, headers: headers)
+        .to_return(status: 200, body: { token: 'token123' }.to_json)
 
-        headers
-        post :submission_token, params: { source_code: "print('hello')", language_id: 71 }
-      end
-      it 'returns token' do
-        result = json
+      headers
+      post :submission_token, params: { source_code: "print('hello')", language_id: 71 }
+    end
+    it 'returns token' do
+      result = json
 
-        expect(result['data']['token']).to eq('token123')
-        expect(result['message']).to eq(I18n.t('success.message'))
-        expect(response).to have_http_status(200)
-      end
+      expect(result['data']['token']).to eq('token123')
+      expect(result['message']).to eq(I18n.t('success.message'))
+      expect(response).to have_http_status(200)
     end
 
     describe 'GET #submission_status' do
@@ -42,3 +40,4 @@ RSpec.describe Api::V1::ExecutionsController, type: :controller do
     end
   end
 end
+
