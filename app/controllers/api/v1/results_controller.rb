4ce8ec986ceee90b4fa041406eebcbcf5709187b
Samruddhi Deshpande <samruddhideshpande133@gmail.com>
2021-04-07 20:07:08 +0530

    optimized

--- app/controllers/api/v1/results_controller.rb
+++ app/controllers/api/v1/results_controller.rb
@@ -3,11 +3,17 @@
 module Api
   module V1
     class ResultsController < ApiController
+      before_action :find_drive_candidate
       def index
-        drive = Drive.find(params[:drife_id])
-        drives_candidates = drive.drives_candidates
-        render_success(data: serialize_resource(drives_candidates, DrivesCandidateSerializer),
+        render_success(data: serialize_resource(@drives_candidates, DrivesCandidateSerializer),
                        message: I18n.t('success.message', model_name: DrivesCandidate))
+      end
+
+      private
+
+      def find_drive_candidate
+        drive = Drive.find(params[:drife_id])
+        @drives_candidates = drive.drives_candidates
       end
     end
   end
