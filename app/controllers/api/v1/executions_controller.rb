87f6e5a5ec795fbf675ae71612d4f5b3c0891dfc
Samruddhi Deshpande <samruddhideshpande133@gmail.com>
2021-04-08 13:55:42 +0530

    json helper added

--- app/controllers/api/v1/executions_controller.rb
+++ app/controllers/api/v1/executions_controller.rb
@@ -7,15 +7,13 @@ module Api
       include JsonHelpers
       def submission_token
         parameter = { source_code: params[:source_code], language_id: params[:language_id] }
-        response = JudgeZeroApi.new(parameter).post('/submissions/?base64_encoded=false&wait=false')
-        body = json(response)
+        body = json(JudgeZeroApi.new(parameter).post('/submissions/?base64_encoded=false&wait=false'))
         render_success(data: { token: body['token'] }, message: I18n.t('success.message'))
       end
 
       def submission_status
         token = params[:id]
-        response = JudgeZeroApi.new({}).get("/submissions/#{token}")
-        body = json(response)
+        body = json(JudgeZeroApi.new({}).get("/submissions/#{token}"))
         render_success(data: body, message: I18n.t('success.message'))
       end
     end
