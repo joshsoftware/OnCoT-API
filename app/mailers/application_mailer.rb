4d4fa7a4a4c04cc01ee3e25ad3984bce0ffd854f
Somnath <Somnath.surwase@joshsoftware.com>
2021-04-16 14:19:02 +0530

    smtp setup in production

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV['GMAIL_USER_NAME']
+  default from: 'somnath.surwase@joshsoftware.com'
   layout 'mailer'
 end
