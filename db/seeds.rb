10b5019e9a8fbfeb1f3f17400c4b690f8296cf07
Atira Bagwan <atira.bagwan@joshsoftware.com>
2021-04-28 11:41:26 +0530

    used find_or_create_by

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 if Rails.env.development?
-  role = Role.create!(name: 'Admin')
-  organization = Organization.create!(name: 'Josh Software', email: 'josh@joshsoftware.com',
-                                      contact_number: '123456789')
-  user = User.create!(first_name: 'Admin', email: 'admin@example.com', password: 'password',
-                      password_confirmation: 'password', role_id: role.id, organization_id: organization.id)
-  drive = Drive.create!(name: 'Josh Drive', start_time: '2021-04-26 12:00:00', end_time: '2021-04-28 12:00:00',
-                        organization_id: organization.id, duration: 60, created_by_id: user.id,
-                        updated_by_id: user.id)
-  problem = Problem.create!(title: 'Add numbers', created_by_id: user.id, updated_by_id: user.id,
-                            organization_id: organization.id)
+  role = Role.find_or_create_by!(name: 'Admin')
+  organization = Organization.find_or_create_by!(name: 'Josh Software', email: 'josh@joshsoftware.com',
+                                                 contact_number: '123456789')
+  user = User.find_or_create_by!(first_name: 'Admin', email: 'admin@example.com', role_id: role.id,
+                                 organization_id: organization.id)
+  drive = Drive.find_or_create_by!(name: 'Josh Drive', start_time: '2021-04-26 12:00:00', end_time: '2021-04-28 12:00:00',
+                                   organization_id: organization.id, duration: 60, created_by_id: user.id,
+                                   updated_by_id: user.id)
+  problem = Problem.find_or_create_by!(title: 'Add numbers', created_by_id: user.id, updated_by_id: user.id,
+                                       organization_id: organization.id)
 end
