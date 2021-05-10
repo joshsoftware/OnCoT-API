dba07631951be8b3fd71dd3585b192c050aea5fa
Somnath <Somnath.surwase@joshsoftware.com>
2021-05-10 11:12:03 +0530

    used figaro for env vars

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV['GMAIL_USER_NAME']
+  default from: Figaro.env.MAIL_USERNAME
   layout 'mailer'
 end
