a1dd8df824dd39a6f729b09730615062555fa2d2
Atira Bagwan <atira.bagwan@joshsoftware.com>
2021-05-12 13:35:09 +0530

    acceptance specs for api/v1

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'rack-cors'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'rspec_api_documentation'
 end
 
 group :development do
@@ -51,7 +52,6 @@ group :test do
   gem 'factory_bot_rails'
   gem 'rspec-rails'
   gem 'webmock'
-  gem 'rspec_api_documentation'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ spec/acceptance/api/v1/candidate_result_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+resource 'CandidateResult' do
+  get '/api/v1/drives/:drife_id/problems/:problem_id/candidate_results/:id' do
+    parameter :id, 'Candidate id'
+    parameter :drife_id, 'Drive id'
+    parameter :problem_id, 'Problem id'
+    let!(:organization) { create(:organization) }
+    let!(:user) { create(:user) }
+    let!(:problem) do
+      create(:problem, updated_by_id: user.id, created_by_id: user.id,
+                       organization: organization)
+    end
+
+    let!(:drive) do
+      create(:drive, updated_by_id: user.id, created_by_id: user.id,
+                     organization: organization)
+    end
+
+    let!(:candidate) { create(:candidate) }
+    let!(:drives_candidate) { create(:drives_candidate, drive_id: drive.id, candidate_id: candidate.id) }
+    let!(:test_case) do
+      create(:test_case, problem_id: problem.id, input: 'hello', output: 'hello', marks: 4, updated_by_id: user.id,
+                         created_by_id: user.id)
+    end
+    let!(:submission) { create(:submission, problem_id: problem.id, drives_candidate_id: drives_candidate.id) }
+    let!(:test_case_result) do
+      create(:test_case_result, test_case_id: test_case.id, submission_id: submission.id,
+                                is_passed: true)
+    end
+    let!(:candidate_id) { candidate.id }
+    let!(:drife_id) { drive.id }
+    let!(:problem_id) { problem.id }
+    let!(:id) { candidate.id }
+    example 'API which returns passed, failed testcases and code of a candidate' do
+      do_request
+      response = JSON.parse(response_body)
+      expect(response['data']['passed'][0]['id']).to eq(test_case_result.id)
+      expect(response['data']['passed'][0]['input']).to eq('hello')
+      expect(response['data']['passed'][0]['output']).to eq('hello')
+      expect(response['data']['passed'][0]['marks']).to eq(4)
+      expect(response['data']['failed'][0]).to eq(nil)
+    end
+  end
+end
