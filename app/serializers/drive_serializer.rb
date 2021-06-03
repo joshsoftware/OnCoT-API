aa7eb86fe3fa71d324bf7a2a9a6f60a58cdca7ba
Somnath <Somnath.surwase@joshsoftware.com>
2021-06-03 16:45:42 +0530

    drive details add to drive serializer

--- app/serializers/drive_serializer.rb
+++ app/serializers/drive_serializer.rb
@@ -17,6 +17,24 @@
 #  duration        :integer
 #
 class DriveSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :start_time, :end_time, :created_by_id, :updated_by_id, :organization_id
+  attributes :id, :name, :description, :start_time, :end_time, :created_by_id,
+             :updated_by_id, :organization_id, :invitation_sent, :appeared, :total_submissions
   has_many :drives_problems
+
+  def invitation_sent
+    object.candidates.count
+  end
+
+  def appeared
+    object.drives_candidates.where.not(start_time: nil).count
+  end
+
+  def total_submissions
+    drives_candidates = DrivesCandidate.where(drive_id: object.id)
+    count = 0
+    drives_candidates.each do |drives_candidate|
+      count += Submission.where(drives_candidate_id: drives_candidate.id).count
+    end
+    count
+  end
 end
